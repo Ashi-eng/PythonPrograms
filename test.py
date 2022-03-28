2ae4950a0f5c7078eec07df74253c872e0e322f4
Ashi-eng <60838355+Ashi-eng@users.noreply.github.com>
2022-03-28 13:26:10 +0530

    Adding new python code to check for palindrome

---
+++ test.py
@@ -0,0 +1,9 @@
+#check if a string is a palindrome or not
+def checkString(string):
+    newstr = string[::-1]
+    if newstr == string:
+        print("The string is palindrome")
+    else:
+        print("The string is not palindrome")
+
+checkString(input("Enter string: "))
