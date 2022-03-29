b5cde735f55dcdcbbdd75bc07b2d702ed92fa976
Ashi-eng <60838355+Ashi-eng@users.noreply.github.com>
2022-03-29 13:35:24 +0530

    Checking All type of strings

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 #check if a string is a palindrome or not
 def checkString(string):
+    string.lower()
     newstr = string[::-1]
     if newstr == string:
         print("The string is palindrome")
